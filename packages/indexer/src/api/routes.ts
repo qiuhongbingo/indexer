f6755e946d2b6d48e4be6d76b9e3c47a9c78d259
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2024-04-24 12:10:31 -0400

    feat: remove v8 (#8537)

--- packages/indexer/src/api/routes.ts
+++ packages/indexer/src/api/routes.ts
@@ -1398,12 +1398,6 @@ export const setupRoutes = (server: Server) => {
 
   server.route({
     method: "GET",
-    path: "/tokens/v8",
-    options: tokensEndpoints.getTokensV8Options,
-  });
-
-  server.route({
-    method: "GET",
     path: "/tokens/bootstrap/v1",
     options: tokensEndpoints.getTokensBootstrapV1Options,
   });
