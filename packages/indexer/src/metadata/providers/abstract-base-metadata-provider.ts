32c0cbfe0bdf77eab81a65e9dd4ac626200f213e
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2024-04-26 10:36:02 -0400

    Ofir/platf 3497 discuss enable encoded metadata support (#8562)
    * feat: enable encoded image
    
    * feat: add debug log
    
    * feat: add log
    
    * feat: updates logs
    
    * feat: enable logic for redstone
    
    * feat: fix condition

--- packages/indexer/src/metadata/providers/abstract-base-metadata-provider.ts
+++ packages/indexer/src/metadata/providers/abstract-base-metadata-provider.ts
@@ -103,8 +103,7 @@ export abstract class AbstractBaseMetadataProvider {
         try {
           if (
             metadata.imageUrl &&
-            !metadata.imageUrl.startsWith("data:") &&
-            ![690, 17069].includes(config.chainId) &&
+            (!metadata.imageUrl.startsWith("data:") || [690, 17069].includes(config.chainId)) &&
             !metadata.imageMimeType
           ) {
             const _getImageMimeTypeStart = Date.now();
@@ -144,8 +143,7 @@ export abstract class AbstractBaseMetadataProvider {
 
           if (
             metadata.mediaUrl &&
-            !metadata.mediaUrl.startsWith("data:") &&
-            ![690, 17069].includes(config.chainId) &&
+            (!metadata.mediaUrl.startsWith("data:") || [690, 17069].includes(config.chainId)) &&
             !metadata.mediaMimeType
           ) {
             const _getImageMimeTypeStart = Date.now();
