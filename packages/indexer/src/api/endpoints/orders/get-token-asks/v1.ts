643f218b97fd1fe6ae48f00cfde2a1bd46722fd7
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2024-04-30 11:42:58 -0400

    feat: added user asks, add resize image logic (#8502)
    * feat: joi change
    
    * feat: change path
    
    * feat: added endpoint
    
    * feat: added sort by
    
    * feat: cleanup
    
    * feat: change tag
    
    * feat: revert change
    
    * feat: added collection filter
    
    * feat: added user asks, add resize image logic
    
    * feat: added token endpoint
    
    * feat: add more filters
    
    * feat: add more filters
    
    * feat: add missing import
    
    * feat: fix resizeImageUrl joi logic
    
    * feat: fix query
    
    * feat: fix check
    
    * feat: fix response schema
    
    * feat: fix cont

--- packages/indexer/src/api/endpoints/orders/get-token-asks/v1.ts
+++ packages/indexer/src/api/endpoints/orders/get-token-asks/v1.ts
@@ -7,16 +7,23 @@ import Joi from "joi";
 import { redb } from "@/common/db";
 import { logger } from "@/common/logger";
 import { JoiOrder, getJoiOrderObject } from "@/common/joi";
-import { buildContinuation, regex, splitContinuation, toBuffer } from "@/common/utils";
+import {
+  buildContinuation,
+  getNetAmount,
+  regex,
+  splitContinuation,
+  toBuffer,
+} from "@/common/utils";
 
 import { Orders } from "@/utils/orders";
+import _ from "lodash";
 
 const version = "v1";
 
-export const getCollectionBidsV1Options: RouteOptions = {
-  description: "Collection Bids (offers)",
-  notes: "Get a list of bids (offers), filtered by collection.",
-  tags: ["api", "Collections", "marketplace"],
+export const getTokenAsksV1Options: RouteOptions = {
+  description: "Token Asks (listings)",
+  notes: "Get a list of asks (listings), filtered by token.",
+  tags: ["api", "Tokens"],
   plugins: {
     "hapi-swagger": {
       order: 5,
@@ -24,27 +31,24 @@ export const getCollectionBidsV1Options: RouteOptions = {
   },
   validate: {
     params: Joi.object({
-      collectionId: Joi.string()
+      token: Joi.string()
         .lowercase()
+        .pattern(/^0x[a-fA-F0-9]{40}:[0-9]+$/)
         .description(
-          "Filter to a particular collection. Example: `0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63`"
-        ),
+          "The token to get asks for. Example: `0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63:123`"
+        )
+        .required(),
     }),
     query: Joi.object({
-      type: Joi.string()
-        .valid("token", "collection", "attribute", "custom")
-        .description(
-          "Filter to a particular order type. Must be one of `token`, `collection`, `attribute`, `custom`."
-        ),
       includeCriteriaMetadata: Joi.boolean()
         .default(false)
         .description("If true, criteria metadata is included in the response."),
       includeRawData: Joi.boolean()
         .default(false)
         .description("If true, raw data is included in the response."),
-      includeDepth: Joi.boolean()
+      includeDynamicPricing: Joi.boolean()
         .default(false)
-        .description("If true, the depth of each order is included in the response."),
+        .description("If true, dynamic pricing data will be returned in the response."),
       normalizeRoyalties: Joi.boolean()
         .default(false)
         .description("If true, prices will include missing royalties to be added on-top."),
@@ -67,16 +71,19 @@ export const getCollectionBidsV1Options: RouteOptions = {
     schema: Joi.object({
       orders: Joi.array().items(JoiOrder),
       continuation: Joi.string().pattern(regex.base64).allow(null),
-    }).label(`getCollectionBids${version.toUpperCase()}Response`),
+    }).label(`getTokenAsks${version.toUpperCase()}Response`),
     failAction: (_request, _h, error) => {
-      logger.error(`get-collection-bids-${version}-handler`, `Wrong response schema: ${error}`);
+      logger.error(`get-token-asks-${version}-handler`, `Wrong response schema: ${error}`);
       throw error;
     },
   },
   handler: async (request: Request) => {
     const query = request.query as any;
 
-    (query as any).collectionId = request.params.collectionId;
+    const [contract, tokenId] = request.params.token.split(":");
+
+    (query as any).contract = toBuffer(contract);
+    (query as any).tokenId = tokenId;
 
     try {
       const criteriaBuildQuery = Orders.buildCriteriaQueryV2(
@@ -86,40 +93,26 @@ export const getCollectionBidsV1Options: RouteOptions = {
         "token_set_schema_hash"
       );
 
-      const orderTypeJoin = query.type
-        ? `JOIN token_sets ON token_sets.id = orders.token_set_id AND token_sets.schema_hash = orders.token_set_schema_hash`
-        : "";
-
       let baseQuery = `
         SELECT
           contracts.kind AS "contract_kind",
           orders.id,
           orders.kind,
           orders.side,
-          (
-            CASE
-              WHEN orders.fillability_status = 'filled' THEN 'filled'
-              WHEN orders.fillability_status = 'cancelled' THEN 'cancelled'
-              WHEN orders.fillability_status = 'expired' THEN 'expired'
-              WHEN orders.fillability_status = 'no-balance' THEN 'inactive'
-              WHEN orders.approval_status = 'no-approval' THEN 'inactive'
-              WHEN orders.approval_status = 'disabled' THEN 'inactive'
-              ELSE 'active'
-            END
-          ) AS status,
           orders.token_set_id,
           orders.token_set_schema_hash,
           orders.contract,
           orders.maker,
           orders.taker,
+          orders.currency,
           orders.price,
           orders.value,
-          orders.currency,
           orders.currency_price,
           orders.currency_value,
           orders.normalized_value,
           orders.currency_normalized_value,
           orders.missing_royalties,
+          dynamic,
           DATE_PART('epoch', LOWER(orders.valid_between)) AS valid_from,
           COALESCE(
             NULLIF(DATE_PART('epoch', UPPER(orders.valid_between)), 'Infinity'),
@@ -136,12 +129,22 @@ export const getCollectionBidsV1Options: RouteOptions = {
           ) AS expiration,
           orders.is_reservoir,
           extract(epoch from orders.created_at) AS created_at,
+          (
+            CASE
+              WHEN orders.fillability_status = 'filled' THEN 'filled'
+              WHEN orders.fillability_status = 'cancelled' THEN 'cancelled'
+              WHEN orders.fillability_status = 'expired' THEN 'expired'
+              WHEN orders.fillability_status = 'no-balance' THEN 'inactive'
+              WHEN orders.approval_status = 'no-approval' THEN 'inactive'
+              WHEN orders.approval_status = 'disabled' THEN 'inactive'
+              ELSE 'active'
+            END
+          ) AS status,
           extract(epoch from orders.updated_at) AS updated_at,
           orders.originated_at,
           (${criteriaBuildQuery}) AS criteria
-          ${query.includeRawData || query.includeDepth ? ", orders.raw_data" : ""}
+          ${query.includeRawData || query.includeDynamicPricing ? ", orders.raw_data" : ""}
         FROM orders
-        ${orderTypeJoin}
         JOIN LATERAL (
           SELECT kind
           FROM contracts
@@ -151,32 +154,12 @@ export const getCollectionBidsV1Options: RouteOptions = {
 
       // Filters
       const conditions: string[] = [
-        `orders.side = 'buy'`,
+        `orders.side = 'sell'`,
+        `orders.contract = $/contract/`,
         `orders.fillability_status = 'fillable' AND orders.approval_status = 'approved'`,
         `orders.taker = '\\x0000000000000000000000000000000000000000' OR orders.taker IS NULL`,
+        `orders.token_set_id = 'token:${contract}:${tokenId}'`,
       ];
-
-      const [contract] = query.collectionId.split(":");
-
-      (query as any).contract = toBuffer(contract);
-      conditions.push(`orders.contract = $/contract/`);
-
-      if (!query.collectionId.match(regex.address)) {
-        baseQuery += `
-            JOIN LATERAL (
-              SELECT
-                contract,
-                token_id
-              FROM
-                token_sets_tokens
-              WHERE
-                token_sets_tokens.token_set_id = orders.token_set_id LIMIT 1) tst ON TRUE
-            JOIN tokens ON tokens.contract = tst.contract
-              AND tokens.token_id = tst.token_id
-          `;
-
-        conditions.push(`tokens.collection_id = $/collectionId/`);
-      }
 
       switch (query.type) {
         case "token": {
@@ -209,15 +192,21 @@ export const getCollectionBidsV1Options: RouteOptions = {
       }
 
       if (query.continuation) {
-        const [sortOrderValue, sortOrderId] = splitContinuation(
+        const [sortOrderPrice, sortOrderId] = splitContinuation(
           query.continuation,
           /^\d+(.\d+)?_0x[a-f0-9]{64}$/
         );
 
-        (query as any).sortOrderValue = sortOrderValue;
+        (query as any).sortOrderPrice = sortOrderPrice;
         (query as any).sortOrderId = sortOrderId;
 
-        conditions.push(`(orders.value, orders.id) < ($/sortOrderValue/, $/sortOrderId/)`);
+        if (query.normalizeRoyalties) {
+          conditions.push(
+            `(orders.normalized_value, orders.id) > ($/sortOrderPrice/, $/sortOrderId/)`
+          );
+        } else {
+          conditions.push(`(orders.price, orders.id) > ($/sortOrderPrice/, $/sortOrderId/)`);
+        }
       }
 
       if (conditions.length) {
@@ -225,7 +214,11 @@ export const getCollectionBidsV1Options: RouteOptions = {
       }
 
       // Sorting
-      baseQuery += ` ORDER BY orders.value DESC, orders.id DESC`;
+      if (query.normalizeRoyalties) {
+        baseQuery += ` ORDER BY orders.normalized_value, orders.fee_bps, orders.id`;
+      } else {
+        baseQuery += ` ORDER BY orders.price, orders.fee_bps, orders.id`;
+      }
 
       // Pagination
       baseQuery += ` LIMIT $/limit/`;
@@ -235,9 +228,15 @@ export const getCollectionBidsV1Options: RouteOptions = {
       let continuation = null;
 
       if (rawResult.length === query.limit) {
-        continuation = buildContinuation(
-          rawResult[rawResult.length - 1].value + "_" + rawResult[rawResult.length - 1].id
-        );
+        const lastResult = rawResult[rawResult.length - 1];
+
+        if (query.normalizeRoyalties) {
+          continuation = buildContinuation(
+            lastResult.normalized_value ?? lastResult.price + "_" + lastResult.id
+          );
+        } else {
+          continuation = buildContinuation(lastResult.price + "_" + lastResult.id);
+        }
       }
 
       const result = rawResult.map(async (r) =>
@@ -255,14 +254,14 @@ export const getCollectionBidsV1Options: RouteOptions = {
             taker: r.taker,
             prices: {
               gross: {
-                amount: r.currency_price ?? r.price,
-                nativeAmount: r.price,
+                amount: query.normalizeRoyalties
+                  ? r.currency_normalized_value ?? r.price
+                  : r.currency_price ?? r.price,
+                nativeAmount: query.normalizeRoyalties ? r.normalized_value ?? r.price : r.price,
               },
               net: {
-                amount: query.normalizeRoyalties
-                  ? r.currency_normalized_value ?? r.value
-                  : r.currency_value ?? r.value,
-                nativeAmount: query.normalizeRoyalties ? r.normalized_value ?? r.value : r.value,
+                amount: getNetAmount(r.currency_price ?? r.price, _.min([r.fee_bps, 10000])),
+                nativeAmount: getNetAmount(r.price, _.min([r.fee_bps, 10000])),
               },
               currency: r.currency,
             },
@@ -281,11 +280,12 @@ export const getCollectionBidsV1Options: RouteOptions = {
             originatedAt: r.originated_at,
             rawData: r.raw_data,
             missingRoyalties: r.missing_royalties,
+            dynamic: r.dynamic,
           },
           {
             normalizeRoyalties: query.normalizeRoyalties,
             includeRawData: query.includeRawData,
-            includeDepth: query.includeDepth,
+            includeDynamicPricing: query.includeDynamicPricing,
             displayCurrency: query.displayCurrency,
             resizeImageUrl: query.includeCriteriaMetadata,
           }
@@ -297,7 +297,7 @@ export const getCollectionBidsV1Options: RouteOptions = {
         continuation,
       };
     } catch (error) {
-      logger.error(`get-collection-bids-${version}-handler`, `Handler failure: ${error}`);
+      logger.error(`get-token-asks-${version}-handler`, `Handler failure: ${error}`);
       throw error;
     }
   },
