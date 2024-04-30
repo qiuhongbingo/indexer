44b37eb5b8461d939658f09f7c8707f3193673b9
Idan Peleg <ipeleg@hotmail.com>
2024-04-30 09:30:36 -0400

    feat: update apis tag

--- packages/indexer/src/api/endpoints/tokens/get-tokens/v3.ts
+++ packages/indexer/src/api/endpoints/tokens/get-tokens/v3.ts
@@ -24,7 +24,7 @@ export const getTokensV3Options: RouteOptions = {
   description: "List of tokens, with basic details, optimized for speed",
   notes:
     "This API is optimized for quickly fetching a list of tokens in a collection, sorted by price, with only the most important information returned. If you need more metadata, use the `tokens/details` API",
-  tags: ["api", "x-deprecated"],
+  tags: ["api", "x-deprecated", "marketplace"],
   plugins: {
     "hapi-swagger": {
       deprecated: true,
